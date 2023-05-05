9502b7206e0e25c263c2553b837824f54aec2318
nfwongaa <87237668+nfwongaa@users.noreply.github.com>
2023-05-05 17:22:11 +0800

    bugged
    bugged

--- js/unsharpenmask.js
+++ js/unsharpenmask.js
@@ -107,10 +107,10 @@
     }
  
     imageproc.unsharpen = function(inputData, outputData, size, radius, amount, threshold) {
-        console.log("Applying Kuwahara filter...");
+        console.log("Applying Unsharpen filter...");
 
         /*
-         * TODO: You need to extend the kuwahara function to include different
+         * TODO: You need to extend the unsharpen function to include different
          * sizes of the filter
          *
          * You need to clearly understand the following code to make
@@ -163,44 +163,74 @@
 
         for (var y = 0; y < inputData.height; y++) {
             for (var x = 0; x < inputData.width; x++) {
-                // Find the statistics of the four sub-regions
-                var regionA = regionStat(x - parseInt(size/4), y - parseInt(size/4), inputData);
-                var regionB = regionStat(x + parseInt(size/4), y - parseInt(size/4), inputData);
-                var regionC = regionStat(x - parseInt(size/4), y + parseInt(size/4), inputData);
-                var regionD = regionStat(x + parseInt(size/4), y + parseInt(size/4), inputData);
+                var region = regionStat(x, y);
+                var sigma = Math.sqrt(region.variance);
+                var kernel = [];
+                var mean = size/2;
+                var sum = 0.0; // For accumulating the kernel values
 
-                // Get the minimum variance value
-                var minV = Math.min(regionA.variance, regionB.variance,
-                                    regionC.variance, regionD.variance);
+                for (var a = 0; a < size; ++a){ 
+                    var temp = [];
+                    for (var b = 0; b < size; ++b) {
+                        temp.push(parseFloat(Math.exp( -0.5 * (Math.pow((a-mean)/sigma, 2.0) + Math.pow((b-mean)/sigma,2.0)) )
+                                        / (2 * Math.PI * sigma * sigma)));
+                
+                        // Accumulate the kernel values
 
-                var i = (x + y * inputData.width) * 4;
-
-                // Put the mean colour of the region with the minimum
-                // variance in the pixel
-                switch (minV) {
-                case regionA.variance:
-                    outputData.data[i]     = regionA.mean.r;
-                    outputData.data[i + 1] = regionA.mean.g;
-                    outputData.data[i + 2] = regionA.mean.b;
-                    break;
-                case regionB.variance:
-                    outputData.data[i]     = regionB.mean.r;
-                    outputData.data[i + 1] = regionB.mean.g;
-                    outputData.data[i + 2] = regionB.mean.b;
-                    break;
-                case regionC.variance:
-                    outputData.data[i]     = regionC.mean.r;
-                    outputData.data[i + 1] = regionC.mean.g;
-                    outputData.data[i + 2] = regionC.mean.b;
-                    break;
-                case regionD.variance:
-                    outputData.data[i]     = regionD.mean.r;
-                    outputData.data[i + 1] = regionD.mean.g;
-                    outputData.data[i + 2] = regionD.mean.b;
+                        sum += temp[b];
+                    }
+                    kernel.push(temp);
                 }
+
+                // Normalize the kernel
+                for (var a = 0; a < size; ++a) 
+                    for (var b = 0; b < size; ++b)
+                        kernel[a][b] /= sum;
+
+                // Use imageproc.getPixel() to get the pixel values
+                // over the kernel
+                var rValue = 0, gValue = 0, bValue = 0;
+                for (var j = 0-parseInt(size/2); j <= parseInt(size/2); j++) {
+                    for (var i = 0-parseInt(size/2); i <= parseInt(size/2); i++) {
+                        var pixelValues = imageproc.getPixel(inputData, x+i, y+j, "extend");
+                        rValue += pixelValues.r * kernel[j+parseInt(size/2)][i+parseInt(size/2)]; 
+                        gValue += pixelValues.g * kernel[j+parseInt(size/2)][i+parseInt(size/2)]; 
+                        bValue += pixelValues.b * kernel[j+parseInt(size/2)][i+parseInt(size/2)];
+                    }
+                }
+                // Then set the blurred result to the output data
+                
+                var i = (x + y * outputData.width) * 4;
+                outputData.data[i]     = rValue;
+                outputData.data[i + 1] = gValue;
+                outputData.data[i + 2] = bValue;
             }
         }
-    }
+        var buffer = imageproc.createbuffer(outputData);
+        for (var i = 0; i < size; i++){
+            var temp;
+            temp = inputData.data[i] - buffer.data[i];
+            if(temp > threshold)
+                buffer.data[i] += temp;
+            else
+                buffer.data[i] = 0;
 
+            temp = inputData.data[i+1] - buffer.data[i+1];
+            if(temp > threshold)
+                buffer.data[i+1] += temp;
+            else
+                buffer.data[i+1] = 0;
+            
+            temp = inputData.data[i+2] - buffer.data[i+2];
+            if(temp > threshold)
+                buffer.data[i+2] += temp;
+            else
+                buffer.data[i+2] = 0;
+            
+            outputData.data[i] = inputData.data[i] * buffer.data[i];
+            outputData.data[i+1] = inputData.data[i] * buffer.data[i+1];
+            outputData.data[i+2] = inputData.data[i] * buffer.data[i+2];   
+        }
+    }
 
 }(window.imageproc = window.imageproc || {}));
